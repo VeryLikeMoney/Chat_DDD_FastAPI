67b4d937454320305988975045bfdfa42411a086
ego rep <kloler.egor@mail.ru>
2024-07-25 13:17:04 +0300

    initial commit

---
+++ app/domain/exceptions/message.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from domain.exceptions.base import ApplicationException
+
+@dataclass(eq=False)
+class TextTooLongException(ApplicationException):
+    text: str
+    
+    @property
+    def message(self):
+        return f'Слишком длинный тест сообщения "{self.text[:255]}..."'
